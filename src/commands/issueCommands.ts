05c61df2442cf8e74ebf1e41421fd82b02e9635a
He110te4m <i@he110.info>
2022-09-12 15:06:41 +0800

    feat: 添加创建 issue 成功后提示，创建后自动销毁 comment

--- src/commands/issueCommands.ts
+++ src/commands/issueCommands.ts
@@ -1,10 +1,13 @@
-import { type CommentReply, commands, workspace } from 'vscode'
+import { type CommentReply, commands, window, workspace } from 'vscode'
+import { extensionName } from '../const'
 import type { BaseIssueOperator } from '../operators/issues/BaseIssueOperator'
 import { commandIDs } from './const'
 
 export function registeIssueCommands(issueOperator: BaseIssueOperator) {
   return [
-    commands.registerCommand(commandIDs.createIssue, ({ text, thread: { range, uri } }: CommentReply) => {
+    commands.registerCommand(commandIDs.createIssue, async ({ text, thread }: CommentReply) => {
+      const { range, uri } = thread
+
       const doc = workspace.textDocuments.find(doc => doc.uri.fsPath === uri.fsPath)
       if (!doc) {
         return
@@ -12,7 +15,11 @@ export function registeIssueCommands(issueOperator: BaseIssueOperator) {
 
       const code = doc.getText(range)
 
-      issueOperator.createIssue('test', getIssueTemplate(text, code))
+      await issueOperator.createIssue('test', getIssueTemplate(text, code))
+
+      window.showInformationMessage(`[${extensionName}]: create issue success`)
+
+      thread.dispose()
     }),
   ]
 }
