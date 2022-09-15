1625f0ce1b655e72aa15c4e4ba33c0dc7b7dfaa9
He110te4m <i@he110.info>
2022-09-15 11:17:18 +0800

    refactor(style): Deal with the problem of chaotic orientation of the suspended title

--- web/issue/src/components/IssueCode.vue
+++ web/issue/src/components/IssueCode.vue
@@ -4,8 +4,10 @@ const { issue } = defineProps<{ issue: IssueCodeData; }>();
 
 <template>
   <div flex flex-col>
-    <div :title="issue.code" class="ellipsis">
-      {{ issue.path }}
+    <div :title="issue.code">
+      <div class="ellipsis">
+        {{ issue.path }}
+      </div>
     </div>
     <div opacity="50">
       {{ issue.desc }}
